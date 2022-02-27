c5db525402c53c3c95b80e26b9fec17794f346b8
Teck Onn <teckonn.miew@photobookworldwide.com>
2022-02-28 01:55:42 +0800

    wip

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import jobApplications from './routes/jobApplication'
 
 const app = express()
 const port = 3001
 
+app.use(cors())
 app.use(express.json())
 app.use('/', jobApplications)
 
